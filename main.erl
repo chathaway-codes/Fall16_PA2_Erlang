9848ba2e88ecec7e768491d7d0f1c1cf0856614c
bennyty <bennytytyty@gmail.com>
2016-10-28 14:53:27 -0400

    Added case to detect no named nodes
    Students using ./run.sh will not have any named nodes, this will call
    node() in that case.

--- main.erl
+++ main.erl
@@ -1,6 +1,19 @@
 -module(main).
 -compile(export_all).
 
+%Use the following function to get a random node (or the current node if no named nodes exist).
+get_random_node() ->
+  World = net_adm:world(),
+  case length(World) of
+    0 -> node();
+    _ -> rget_random_node(rand:uniform(length(World)), World)
+  end.
+
+rget_random_node(_, []) -> node();
+rget_random_node(1, [H|_]) -> H;
+rget_random_node(_, [H|[]]) -> H;
+rget_random_node(N, [_|T]) -> rget_random_node(N-1, T).
+
 do_work(StartSeq, TargetSeq, PID) ->
   PID ! {ok, 4}.
 
